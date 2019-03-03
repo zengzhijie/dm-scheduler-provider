ab298afa5039adb3be33c0e487c8ac6018b9c128
曾志杰 <zengzhijie>
2019-03-03 14:36:40 +0800

    1

---
+++ src/main/java/cn/dm/DmSchedulerProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmSchedulerProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmSchedulerProviderApplication.class, args);
+    }
+
+}
